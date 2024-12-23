563d8e888e4e8657168f8bc3c29c6c109adfed8f
Bittu_Sharma <deepcode143@gmail.com>
2024-12-24 04:54:02 +0530

    nav code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Nav></Nav>
       <h1>this is a static website</h1>
       <h1>welcome friends this is my static website</h1>
     </>
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+
+function Nav(){
+    
+    return(
+        <>
+            <h1>this is navbar</h1>
+        </>
+    )
+}
+
+export default Nav;
